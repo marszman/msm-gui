f3002a755200c9a4b352787eaa299d1112002ceb
marszman <136658905+marszman@users.noreply.github.com>
2023-08-01 21:14:31 +0000

    msm-gui - movies complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,56 @@
 class MoviesController < ApplicationController
+  
+  def update
+
+    m_id = params.fetch("the_id")
+
+    matching_records = Movie.where({ :id => m_id })
+
+    m = matching_records.at(0)
+
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies/#{m.id}")
+
+  end
+  
+  def create
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+
+  end
+
+  def destroy
+  
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+
+  end
+    
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
